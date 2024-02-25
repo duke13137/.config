11dd648fe097091dd6ceb0d81419ef530a2d6eac
user <user@local.domain>
2024-02-25 10:26:34 -0500

    cleanup haskell

--- nvim/lua/plugins/lazyvim.lua
+++ nvim/lua/plugins/lazyvim.lua
@@ -115,10 +115,15 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
-        formatters_by_ft = {
-        -- haskell = { "fourmolu" },
+      formatters_by_ft = {
+        haskell = { "stylishhaskell" },
         sh = { "shellcheck" },
       },
+      formatters = {
+        stylishhaskell = {
+          command = "stylish-haskell", args = { "-i" },
+        }
+      }
     }
   },
 
