1f04bc907b599b6d910367316782966d0d06925b
user <user@local.domain>
2024-05-17 17:15:04 -0400

    update

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -13,7 +13,6 @@ require("lazy").setup({
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.dap.nlua" },
-    { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
