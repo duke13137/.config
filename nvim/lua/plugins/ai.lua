49f4831156a72f2373d7bb47ca23420d16ce496b
Duke <duke@home.com>
2025-01-18 12:42:46 -0500

    nvim

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -5,12 +5,12 @@ return {
       "AiderTerminalToggle",
     },
     keys = {
+      { "<leader>aa", "<cmd>AiderQuickAddFile<cr>", desc = "Aider: Add File" },
+      { "<leader>ad", "<cmd>AiderQuickDropFile<cr>", desc = "Aider: Drop File" },
       { "<leader>ai", "<cmd>AiderTerminalToggle<cr>", desc = "Aider: Open Terminal " },
       { "<leader>al", "<cmd>AiderTerminalSend<cr>", desc = "Aider: Send", mode = { "n", "v" } },
+      { "<leader>aL", "<cmd>AiderQuickSendBuffer<cr>", desc = "Aider: Send Buffer" },
       { "<leader>ak", "<cmd>AiderQuickSendCommand<cr>", desc = "Aider: Send Command" },
-      { "<leader>aj", "<cmd>AiderQuickSendBuffer<cr>", desc = "Aider: Send Buffer" },
-      { "<leader>a+", "<cmd>AiderQuickAddFile<cr>", desc = "Aider: Add File" },
-      { "<leader>a-", "<cmd>AiderQuickDropFile<cr>", desc = "Aider: Drop File" },
     },
     dependencies = {
       "nvim-telescope/telescope.nvim",
@@ -23,6 +23,7 @@ return {
         "--watch-files",
       },
       win = {
+        position = "right",
         wo = { winbar = "Aider" },
       },
     },
--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -53,7 +53,7 @@ function M.config()
     insert_mappings = true, -- whether or not the open mapping applies in insert mode
     persist_size = true,
     persist_mode = true,
-    direction = "vertical", -- 'vertical' | 'horizontal' | 'window' | 'float',
+    direction = "float", -- 'vertical' | 'horizontal' | 'window' | 'float',
     close_on_exit = true, -- close the terminal window when the process exits
     shell = vim.o.shell, -- change the default shell
     -- This field is only relevant if direction is set to 'float'
