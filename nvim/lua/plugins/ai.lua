35658ca5438f48fc1f24d21ecb8b5600fe92cb60
Duke <duke@home.com>
2025-04-13 20:07:43 -0400

    update aider

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -4,30 +4,32 @@ return {
     init = function()
       vim.g.llama_config = {
         auto_fim = true,
-        show_info = false,
+        show_info = 1,
+        keymap_trigger = "<C-l>",
+        keymap_accept_line = "<Tab>",
+        keymap_accept_full = "<S-Tab>",
+        keymap_accept_word = "",
       }
     end,
   },
   {
     "GeorgesAlkhouri/nvim-aider",
-    cmd = {
-      "AiderTerminalToggle",
-    },
+    cmd = "Aider",
     keys = {
-      { "<leader>aa", "<cmd>AiderQuickAddFile<cr>", desc = "Aider: Add File" },
-      { "<leader>ad", "<cmd>AiderQuickDropFile<cr>", desc = "Aider: Drop File" },
-      { "<leader>ai", "<cmd>AiderTerminalToggle<cr>", desc = "Aider: Open Terminal " },
-      { "<leader>al", "<cmd>AiderTerminalSend<cr>", desc = "Aider: Send", mode = { "n", "v" } },
-      { "<leader>aL", "<cmd>AiderQuickSendBuffer<cr>", desc = "Aider: Send Buffer" },
-      { "<leader>ak", "<cmd>AiderQuickSendCommand<cr>", desc = "Aider: Send Command" },
+      { "<leader>aa", "<cmd>Aider add<cr>", desc = "Aider: Add File" },
+      { "<leader>ab", "<cmd>Aider buffer<cr>", desc = "Aider: Send Buffer" },
+      { "<leader>ac", "<cmd>Aider command<cr>", desc = "Aider: Send Command" },
+      { "<leader>ad", "<cmd>Aider drop<cr>", desc = "Aider: Drop File" },
+      { "<leader>as", "<cmd>Aider send<cr>", desc = "Aider: Send", mode = { "n", "v" } },
+      { "<leader>at", "<cmd>Aider toggle<cr>", desc = "Aider: Open Terminal " },
     },
     dependencies = {
-      "nvim-telescope/telescope.nvim",
+      "folke/snacks.nvim",
     },
     opts = {
       args = {
         "--model",
-        "g2pro",
+        "gemini",
         "--no-analytics",
         "--no-auto-commits",
         "--yes-always",
