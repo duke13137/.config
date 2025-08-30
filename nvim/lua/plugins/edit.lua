d0d3fab070b3279d1502dd039265490ee19ec955
Duke <duke@home.com>
2025-08-30 00:20:33 -0400

    use Duke's LazyVim

---
+++ nvim/lua/plugins/edit.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    "MagicDuck/grug-far.nvim",
+    opts = {
+      keymaps = {
+        replace = { n = "gr" },
+        qflist = { n = "gq" },
+        syncLocations = { n = "gs" },
+        syncLine = { n = "gl" },
+        close = { n = "gc" },
+        historyOpen = { n = "gt" },
+        historyAdd = { n = "ga" },
+        refresh = { n = "gf" },
+        openLocation = { n = "go" },
+        openNextLocation = { n = "<down>" },
+        openPrevLocation = { n = "<up>" },
+        gotoLocation = { n = "<enter>" },
+        pickHistoryEntry = { n = "<enter>" },
+        abort = { n = "gb" },
+        help = { n = "g?" },
+        toggleShowCommand = { n = "gp" },
+        swapEngine = { n = "ge" },
+        previewLocation = { n = "gi" },
+        swapReplacementInterpreter = { n = "gx" },
+        applyNext = { n = "gj" },
+        applyPrev = { n = "gk" },
+      },
+    },
+  },
+
+  {
+    "nvim-mini/mini.surround",
+    opts = {
+      mappings = {
+        add = "gsa",
+        delete = "gsd",
+        replace = "gsr",
+        find = "gsf",
+        find_left = "gsF",
+        highlight = "gsh",
+        update_n_lines = "gsn",
+      },
+    },
+  },
+}
