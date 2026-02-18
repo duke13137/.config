b8bb46ea08314516149158bdca4857a193a9fdcb
Duke <duke@home.com>
2026-02-17 20:55:52 -0500

    update

--- nvim/lua/plugins/haskell.lua
+++ nvim/lua/plugins/haskell.lua
@@ -40,19 +40,6 @@ local M = {
     },
   },
   {
-    "luc-tielen/telescope_hoogle",
-    ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
-    dependencies = {
-      { "nvim-telescope/telescope.nvim" },
-    },
-    config = function()
-      local ok, telescope = pcall(require, "telescope")
-      if ok then
-        telescope.load_extension("hoogle")
-      end
-    end,
-  },
-  {
     "fonghou/fzf-hoogle.vim",
     ft = "haskell",
     dependencies = {
